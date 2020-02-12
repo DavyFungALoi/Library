a94041996dfb38ec07c3589859b80788e6799c52
Davy <davyfungaloi@gmail.com>
2020-02-12 07:32:24 +0100

    Managed to get JS display HTML on the page with new books

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let library = []
 
 class createBook {
-    constructor(author, title, numberOfPages) {
+    constructor(author, title, pages) {
         this._author = author;
         this._title = title;
-        this._numberOfPages = numberOfPages;
+        this._pages = pages;
         this._isRead = false
         }
         get author () {
@@ -13,8 +13,8 @@ class createBook {
         get title () {
         return this._title
     }
-        get numberOfPages () {
-        return this._numberOfPages
+        get pages () {
+        return this._pages
     }
         get readStatus () {
         return this._isRead
@@ -25,16 +25,36 @@ class createBook {
         togglereadStatus() {
         this.readStatus = !this.readStatus
         }
-
+     
         
 }
-let newBook = new createBook ('Man', 'made', 575)
 
-function addBookToLibrary() {
-    library.push('newBook')
+let newBook = new createBook ('Man', 'made', '575') 
+let newBook2 = new createBook ('Kappa', "Kappa2", "697")
+let newBook3 = new createBook ('Krappa', "Krappa2", "652")
+function addBookToLibrary1() {
+    library.push(newBook)
+    library.push(newBook2)
+    library.push(newBook3)
+    
   }
-  console.log(library)
-  console.log(newBook.author)
-  console.log(newBook.readStatus)
-  newBook.togglereadStatus()
-  console.log(newBook.readStatus)
+  addBookToLibrary1()
+console.log(library)
+
+
+function addBookToLibrary(book) {
+for (i=0; i<library.length;i++)
+{
+
+ let bookinfo = document.createElement("div")
+ bookinfo.classList.add('book-overview')
+ bookinfo.textContent =library[i].author + " " + library[i].title + " " + library[i].pages
+ document.getElementById("bookcontainer").appendChild(bookinfo)
+ 
+}
+}
+console.log(library.length)
+addBookToLibrary()
+
+
+
